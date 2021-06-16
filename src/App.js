01d56cc429c707c2d6b98d8b8743576ec13809ed
laherasif <47274948+laherasif@users.noreply.github.com>
2021-06-15 21:42:36 -0700

    blockchain
    New blockchain project in react js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from './logo.svg';
+// import './App.css';
+import Web3 from 'web3'
+import React,{ useEffect , useState } from 'react'
 function App() {
+  const [account, setaccount] = useState("")
+  const [Balance, setBalance] = useState("")
+  const [Trans, setTrans] = useState(0)
+useEffect(() => {
+ 
+   const loadBlockchainData = async ()  =>  {
+
+    const { ethereum } = window;
+    if (!ethereum) {
+      throw new Error("Web3 not found");
+    }
+    const web3 = new Web3(ethereum);
+    await ethereum.enable();
+  
+    const accounts = await web3.eth.getAccounts();
+    const Balnce = await web3.eth.getBalance(account , (error , result) => {
+    if(error){
+      console.log("error", error);
+    }
+    else {
+      console.log("result " , result );
+    }
+    })
+
+    const NumCountReq = await web3.eth.getTransactionCount(account)
+    const netId = await web3.eth.net.getId();
+
+    for (let i = 1; i <= 10; i++) {
+      const txIndex = NumCountReq - i;
+      if (txIndex < 0) {
+        break;
+      }
+    const tx = await web3.eth.getTransaction(txIndex);
+    const isConfirmed = await web3.eth.isConfirmed(txIndex, account);
+    console.log("txt first" , tx);
+    console.log("txt" , isConfirmed);
+    
+    }
+        console.log("data",accounts);
+        console.log("data",Balnce);
+        console.log("data",NumCountReq);
+        console.log("data",netId);
+
+
+        
+    setaccount( accounts[0] )
+    setBalance( Balnce )
+    setTrans(NumCountReq)
+  }
+  loadBlockchainData()
+  
+}, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Account : { account}
+      <br/>
+      Balance : { Balance }
+       
+      <br/>
+      Transection : { Trans }
     </div>
   );
 }
